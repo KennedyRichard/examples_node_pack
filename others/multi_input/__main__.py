6f0192af38e40bc05407561b4324fd895c476138
Kennedy Richard <kennedyrichard3000@gmail.com>
2022-06-21 16:18:18 -0300

    Add files via upload

---
+++ others/multi_input/__main__.py
@@ -0,0 +1,23 @@
+"""Facility for simple node test."""
+
+from collections.abc import Iterable, Iterator
+
+def multi_input(
+    a_list               : list,
+    a_tuple              : tuple,
+    a_dict               : dict,
+    a_boolean            : bool,
+    a_string             : str,
+    number               : int,
+    also_a_number        : float,
+    iterable             : Iterable,
+    iterator             : Iterator,
+    other_type           : range,
+    type_not_specified,
+    ):
+    """Print obj, then return it."""
+    pass
+
+### the callable used must always be aliased as
+### 'main'
+main_callable = multi_input
